7b8f2a8748294b6496af0e18927b31a728b86d89
BESHMASHAREN <beshmasharen2013@gmail.com>
2026-06-28 18:35:32 +0530

    Upload Calculator project files
    Added index.html, style.css, and script.js files for the Modern Glass Calculator project.

---
+++ Calculator/script.js
@@ -0,0 +1,53 @@
+let display=document.getElementById("display");
+
+function append(value){
+
+display.value+=value;
+
+}
+
+function clearDisplay(){
+
+display.value="";
+
+}
+
+function deleteLast(){
+
+display.value=display.value.slice(0,-1);
+
+}
+
+function calculate(){
+
+try{
+
+display.value=eval(display.value);
+
+}
+
+catch{
+
+display.value="Error";
+
+}
+
+}
+
+// Keyboard Support
+
+document.addEventListener("keydown",(e)=>{
+
+if((e.key>='0'&&e.key<='9')||['+','-','*','/','.','%'].includes(e.key))
+display.value+=e.key;
+
+if(e.key==="Enter")
+calculate();
+
+if(e.key==="Backspace")
+deleteLast();
+
+if(e.key==="Escape")
+clearDisplay();
+
+});
